6f8eb3d0e26fec8e555c8658799d39b48a352802
Shubham Mishra <shubhammishra2751@gmail.com>
2021-05-14 16:16:34 +0530

    third commit

--- controller/connection.php
+++ controller/connection.php
@@ -1,16 +1,16 @@
 <?php
     function connect(){
-        $dbHost= "localhost";
-        // here mysql port is 3306
-        $dbHost = "localhost";
-        $user= "root";
-        $pass= "Mishra@2000";
-        $dbname="inventory_project";
+        // $dbHost= "localhost";
+        // // here mysql port is 3306
+        // $dbHost = "localhost";
+        // $user= "root";
+        // $pass= "Mishra@2000";
+        // $dbname="inventory_project";
 
-        // $dbHost = "remotemysql.com";
-        // $user= "u96YGvXCdt";
-        // $pass= "8lPpENpF7I";
-        // $dbname="u96YGvXCdt";
+        $dbHost = "remotemysql.com";
+        $user= "u96YGvXCdt";
+        $pass= "8lPpENpF7I";
+        $dbname="u96YGvXCdt";
         
 
         $conn= new mysqli($dbHost, $user, $pass, $dbname);
